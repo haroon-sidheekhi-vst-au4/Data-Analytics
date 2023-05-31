5a62057a70e8b952cdc087a04fca0ee203aaa9b0
haroonsidheekhi <haroonsidheekhih@gmail.com>
2023-06-01 01:28:20 +0530

    ...

---
+++ Projects/tracking data/2/file02.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 24 19:49:40 2023
+
+@author: hp
+"""
+
+import Metrica_IO as mio
+import Metrica_Viz as mviz
+import Metrica_Velocities as mvel
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+# set up initial path to data
+DATADIR = 'E:/Docs/Data/metrica sports sample-data-master/data'
+game_id = 2 # let's look at sample match 2
+
+# read in the event data
+events = mio.read_event_data(DATADIR,game_id)
+
+# read in tracking data
+tracking_home = mio.tracking_data(DATADIR,game_id,'Home')
+tracking_away = mio.tracking_data(DATADIR,game_id,'Away')
+
+# Convert positions from metrica units to meters (note change in Metrica's coordinate system since the last lesson)
+tracking_home = mio.to_metric_coordinates(tracking_home)
+tracking_away = mio.to_metric_coordinates(tracking_away)
+events = mio.to_metric_coordinates(events)
+
+tracking_home,tracking_away,events = mio.to_single_playing_direction(tracking_home,tracking_away,events)
+
+
+### movei of first goal
+PLOTDIR = DATADIR
+#mviz.save_match_clip(tracking_home.iloc[11600:11600+650],tracking_away.iloc[11600:11600+650],PLOTDIR,fname='home_goal_2',include_player_velocities=False)
+
+tracking_home = mvel.calc_player_velocities(tracking_home,smoothing=True,filter_='moving_average')
+tracking_away = mvel.calc_player_velocities(tracking_away,smoothing=True,filter_='moving_average')
+
+### Viz with velocity
+
+mviz.plot_frame( tracking_home.loc[11000], tracking_away.loc[11000], include_player_velocities=True, annotate=True)
+
+
+home_players = np.unique( [ c.split('_')[1] for c in tracking_home.columns if c[:4] == 'Home' ] )
+home_summary = pd.DataFrame(index=home_players)
+
+# minutes played 
+minutes = []
+for player in home_players:
+    # search for first and last frames that we have a position observation for each player (when a player is not on the pitch positions are NaN)
+    column = 'Home_' + player + '_x' # use player x-position coordinate
+    player_minutes = ( tracking_home[column].last_valid_index() - tracking_home[column].first_valid_index() + 1 ) / 25 / 60. # convert to minutes
+    minutes.append( player_minutes )
+home_summary['Minutes Played'] = minutes
+home_summary = home_summary.sort_values(['Minutes Played'], ascending=False)
+
+# Total distance covered
+distance = []
+for player in home_summary.index:
+    column = 'Home_' + player + '_speed'
+    player_distance = tracking_home[column].sum()/25./1000 # this is the sum of the distance travelled from one observation to the next (1/25 = 40ms) in km.
+    distance.append( player_distance )
+home_summary['Distance [km]'] = distance
+
+plt.subplots()
+ax = home_summary['Distance [km]'].plot.bar(rot=0)
+ax.set_xlabel('Player')
+ax.set_ylabel('Distance covered [km]')
+
+#distance covered while: walking, joggings, running, sprinting
+
+walking = []
+jogging = []
+running = []
+sprinting = []
+for player in home_summary.index:
+    column = 'Home_' + player + '_speed'
+    # walking (less than 2 m/s)
+    player_distance = tracking_home.loc[tracking_home[column] < 2, column].sum()/25./1000
+    walking.append( player_distance )
+    # jogging (between 2 and 4 m/s)
+    player_distance = tracking_home.loc[ (tracking_home[column] >= 2) & (tracking_home[column] < 4), column].sum()/25./1000
+    jogging.append( player_distance )
+    # running (between 4 and 7 m/s)
+    player_distance = tracking_home.loc[ (tracking_home[column] >= 4) & (tracking_home[column] < 7), column].sum()/25./1000
+    running.append( player_distance )
+    # sprinting (greater than 7 m/s)
+    player_distance = tracking_home.loc[ tracking_home[column] >= 7, column].sum()/25./1000
+    sprinting.append( player_distance )
+    
+home_summary['Walking [km]'] = walking
+home_summary['Jogging [km]'] = jogging
+home_summary['Running [km]'] = running
+home_summary['Sprinting [km]'] = sprinting
+
+ax = home_summary[['Walking [km]','Jogging [km]','Running [km]','Sprinting [km]']].plot.bar(colormap='coolwarm')
+ax.set_xlabel('Player')
+ax.set_ylabel('Distance covered [km]')
+
+
+#Number of sprints made
+
+nsprints = []
+sprint_threshold = 7 # minimum speed to be defined as a sprint (m/s)
+sprint_window = 1*25 # minimum duration sprint should be sustained (in this case, 1 second = 25 consecutive frames)
+for player in home_summary.index:
+    column = 'Home_' + player + '_speed'
+    # trick here is to convolve speed with a window of size 'sprint_window', and find number of occassions that sprint was sustained for at least one window length
+    # diff helps us to identify when the window starts
+    player_sprints = np.diff( 1*( np.convolve( 1*(tracking_home[column]>=sprint_threshold), np.ones(sprint_window), mode='same' ) >= sprint_window ) )
+    nsprints.append( np.sum( player_sprints == 1 ) )
+home_summary['# sprints'] = nsprints
+
+# Viz of each Sprints made by player
+
+player = '7'
+column = 'Home_' + player + '_speed' # spped
+column_x = 'Home_' + player + '_x' # x position
+column_y = 'Home_' + player + '_y' # y position
+# same trick as before to find start and end indices of windows of size 'sprint_window' in which player speed was above the sprint_threshold
+player_sprints = np.diff( 1*( np.convolve( 1*(tracking_home[column]>=sprint_threshold), np.ones(sprint_window), mode='same' ) >= sprint_window ) )
+player_sprints_start = np.where( player_sprints == 1 )[0] - int(sprint_window/2) + 1 # adding sprint_window/2 because of the way that the convolution is centred
+player_sprints_end = np.where( player_sprints == -1 )[0] + int(sprint_window/2) + 1
+# now plot all the sprints
+fig,ax = mviz.plot_pitch()
+for s,e in zip(player_sprints_start,player_sprints_end):
+    ax.plot(tracking_home[column_x].iloc[s],tracking_home[column_y].iloc[s],'ro')
+    ax.plot(tracking_home[column_x].iloc[s:e+1],tracking_home[column_y].iloc[s:e+1],'r')
+   
