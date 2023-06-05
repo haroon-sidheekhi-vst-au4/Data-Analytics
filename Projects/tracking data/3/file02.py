e45ee75e323e75ad4f6197d7d294131025241870
haroonsidheekhi <haroonsidheekhih@gmail.com>
2023-06-05 21:36:34 +0530

    ..

---
+++ Projects/tracking data/3/file02.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 25 11:50:08 2023
+
+@author: hp
+"""
+
+
+import Metrica_IO as mio
+import Metrica_Viz as mviz
+import Metrica_Velocities as mvel
+import Metrica_PitchControl as mpc
+import numpy as np
+
+DATADIR = 'E:/Docs/Data/metrica sports sample-data-master/data'
+game_id = 2 # let's look at sample match 2
+
+# read in the event data
+events = mio.read_event_data(DATADIR,game_id)
+
+# read in tracking data
+tracking_home = mio.tracking_data(DATADIR,game_id,'Home')
+tracking_away = mio.tracking_data(DATADIR,game_id,'Away')
+
+# Convert positions from metrica units to meters (note change in Metrica's coordinate system since the last lesson)
+tracking_home = mio.to_metric_coordinates(tracking_home)
+tracking_away = mio.to_metric_coordinates(tracking_away)
+events = mio.to_metric_coordinates(events)
+
+# reverse direction of play in the second half so that home team is always attacking from right->left
+tracking_home,tracking_away,events = mio.to_single_playing_direction(tracking_home,tracking_away,events)
+
+tracking_home = mvel.calc_player_velocities(tracking_home,smoothing=True,filter_='moving_average')
+tracking_away = mvel.calc_player_velocities(tracking_away,smoothing=True,filter_='moving_average')
+
+""" **** pitch control **** """
+
+# get all shots and goals in the match
+shots = events[events['Type']=='SHOT']
+goals = shots[shots['Subtype'].str.contains('-GOAL')].copy()
+
+mviz.plot_events( events.loc[1722:1723], color='k', indicators = ['Marker','Arrow'], annotate=True )
+
+##Params
+
+params = mpc.default_model_params()
+# find goalkeepers for offside calculation
+GK_numbers = [mio.find_goalkeeper(tracking_home),mio.find_goalkeeper(tracking_away)]
+
+#PPCF,xgrid,ygrid = mpc.generate_pitch_control_for_event(1719, events, tracking_home, tracking_away, params, GK_numbers, field_dimen = (106.,68.,), n_grid_cells_x = 50)
+#mviz.plot_pitchcontrol_for_event( 1719, events,  tracking_home, tracking_away, PPCF, annotate=True )
+
+#PPCF,xgrid,ygrid = mpc.generate_pitch_control_for_event(1720, events, tracking_home, tracking_away, params, GK_numbers, field_dimen = (106.,68.,), n_grid_cells_x = 50)
+#mviz.plot_pitchcontrol_for_event(1720, events,  tracking_home, tracking_away, PPCF, annotate=True )
+
+#PPCF,xgrid,ygrid = mpc.generate_pitch_control_for_event(1722, events, tracking_home, tracking_away, params, GK_numbers, field_dimen = (106.,68.,), n_grid_cells_x = 50)
+#mviz.plot_pitchcontrol_for_event(1722, events,  tracking_home, tracking_away, PPCF, annotate=True )
+
+home_passes = events[ (events['Type'].isin(['PASS'])) & (events['Team']=='Home') ]
+
+# list for storing pass probablities
+pass_success_probability = []
+
+for i,row in home_passes.iterrows():
+    pass_start_pos = np.array([row['Start X'],row['Start Y']])
+    pass_target_pos = np.array([row['End X'],row['End Y']])
+    pass_frame = row['Start Frame']
+    
+    attacking_players = mpc.initialise_players(tracking_home.loc[pass_frame],'Home',params, GK_numbers[0])
+    defending_players = mpc.initialise_players(tracking_away.loc[pass_frame],'Away',params, GK_numbers[1])
+    Patt,Pdef = mpc.calculate_pitch_control_at_target(pass_target_pos, attacking_players, defending_players, pass_start_pos, params)
+
+    pass_success_probability.append( (i,Patt) )
+
+import matplotlib.pyplot as plt
+fig,ax = plt.subplots()
+ax.hist( [p[1] for p in pass_success_probability], np.arange(0,1.1,0.1))    
+ax.set_xlabel('Pass success probability')
+ax.set_ylabel('Frequency')  
+
+pass_success_probability = sorted( pass_success_probability, key = lambda x: x[1] )
+
+# identify the events corresponding to the most risky passes (pitch control < 0.5)
+risky_passes = events.loc[ [p[0] for p in pass_success_probability if p[1]<0.5 ] ]
+
+# plot the events
+mviz.plot_events( risky_passes, color='k', indicators = ['Marker','Arrow'], annotate=True )
+
+print("Event following a risky (completed) pass")
+for p in pass_success_probability[:20]:
+    outcome = events.loc[ p[0]+1 ].Type
+    print( p[1], outcome )
